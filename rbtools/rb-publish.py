54d2f49b306aec91d4ba8b69df54840ecea6f134
Lindsey Sawatzky <dionyses@gmail.com>
2010-10-03 11:28:25 -0400

    Added the rb-delete and rb-publish standalone scripts which will use the API we are developing.

---
+++ rbtools/rb-publish.py
@@ -0,0 +1,23 @@
+#import API STUFF
+
+rb_server_url = "http://reviews.reviewboard.org"
+local_repository = Repository.Repository(rb_server_url, '')
+
+rb_interface = RBInterface.RBInterface()
+server_mgr = ServerManager.ServerManager(rb_interface)
+
+#CONSIDERATION:  should login() return a success/error code, for a more specific indication of what went wrong 
+if !server_mgr.login(rb_server_url, local_repository)
+    print "Login unsuccessfull!"
+
+
+#Make a new review request object.  Note that first creating a review request creates a draft.  This will have to be published eventually.
+review_request = server_mgr.CreateNewReviewRequest(local_repository)
+
+#Set any fields that need to be set
+review_request.set_field(<some field name>, <some field data>)
+#...
+
+#Finally, publish the review request
+review_request.publish()  
+
